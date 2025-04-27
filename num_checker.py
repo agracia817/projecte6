27ca4bd474fc00ad46e30b36b65cfef463591e0f
agracia817 <adriasab@outlook.es>
2025-04-28 00:33:56 +0200

    Add files via upload

---
+++ num_checker.py
@@ -0,0 +1,12 @@
+'''
+DESCRIPCIÓ: Aquest programa comprova si un número enter és parell o senar.
+AUTOR: Adrià Garacia Sabaté
+VERSIO: 1.0
+DATA: 27-04-2025
+'''
+
+num=int(input('introdueix un valor enter: '))
+if num%2==0:
+    print('el valor:',num,'es parell')
+else:
+    print('el valor:',num,'es senar')
---
+++ valor_canviant.py
@@ -0,0 +1,9 @@
+'''
+AUTOR: Adria Gracia Sabate
+VERSIO: 1.0
+DATA: 27-04-2025
+DESCRIPCIO: Programa que demana un valor i el mostra per pantalla.
+'''
+while True :
+    valor=input('posa un valor: ')
+    print(valor)
